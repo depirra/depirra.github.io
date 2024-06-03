63b70d95ac855b47c04d3c30acd37e7d10272fae
depirra <deviraagstn@gmail.com>
2024-06-03 20:25:58 +0700

    tst

--- handler.php
+++ handler.php
@@ -1,4 +1,4 @@
-<?php 
+ <?php 
 if(isset($_POST['submit'])){
     $to = "naufalrafif4345@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
